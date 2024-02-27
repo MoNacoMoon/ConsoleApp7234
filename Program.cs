a6ea55ed6ff22ec624295975e40f5420dc33c513
evol_ <evol_@HP>
2024-02-27 19:55:47 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random random = new Random();
+            //int[] array = new int[6];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = random.Next(1, 11);
+            //}
+
+
+            //Console.WriteLine("Массив случайных чисел:");
+            //foreach (var num in array)
+            //{
+            //    Console.Write(num + " ");
+            //}
+            //Console.WriteLine();
+
+            //int firstElement = array[0];
+            //bool found = false;
+            //for (int i = 1; i < array.Length; i++)
+            //{
+            //    if (array[i] == firstElement)
+            //    {
+            //        Console.WriteLine($"Первый элемент массива ({firstElement}) найден на позиции {i + 1}.");
+            //        found = true;
+            //        break;
+            //    }
+            //}
+
+            //if (!found)
+            //{
+            //    Console.WriteLine("В массиве нет элемента, равного первому элементу.");
+            //}
+
+
+
+            //int[] array = new int[6] { 1, -2, 3, 0, -5, 4 };
+
+            // a) Вывод всех неотрицательных элементов
+            //Console.WriteLine("Неотрицательные элементы:");
+            //foreach (var num in array)
+            //{
+            //    if (num >= 0)
+            //    {
+            //        Console.Write(num + " ");
+            //    }
+            //}
+            //Console.WriteLine();
+
+            // б) Вывод всех элементов, не превышающих число 100
+            //Console.WriteLine("Элементы, не превышающие 100:");
+            //foreach (var num in array)
+            //{
+            //    if (num <= 100)
+            //    {
+            //        Console.Write(num + " ");
+            //    }
+            //}
+            //Console.WriteLine();
+
+            // в) Вывод всех четных элементов
+            //Console.WriteLine("Четные элементы:");
+            //foreach (var num in array)
+            //{
+            //    if (num % 2 == 0)
+            //    {
+            //        Console.Write(num + " ");
+            //    }
+            //}
+            //Console.WriteLine();
+
+
+            //int[] array = { 5, -3, 4, -8, -9, 1, 9, 6, -3, 4, 8, 2 };
+
+            //int negativeCount = 0;
+            //foreach (var num in array)
+            //{
+            //    if (num < 0)
+            //    {
+            //        negativeCount++;
+            //    }
+            //}
+            //Console.WriteLine($"Количество отрицательных элементов: {negativeCount}");
+
+
+
+            //int[] array = new int[6] { 1, 2, 3, 4, 5, 6 };
+
+            //double sum = 0;
+            //int count = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        sum += array[i];
+            //        count++;
+            //    }
+            //}
+
+            //double average = sum / count;
+
+
+            //Console.WriteLine($"Среднее арифметическое элементов с нечетными номерами: {average}");
+
+            //int[] array = new int[6] { 1, 2, 3, 4, 5, 6 };
+
+            //// Увеличение всех элементов массива в 5 раз
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] *= 5;
+            //}
+
+            //// Вывод элементов массива
+            //foreach (var num in array)
+            //{
+            //    Console.Write(num + " ");
+            //}
+            //Console.WriteLine();
+
+            //Console.ReadKey();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
